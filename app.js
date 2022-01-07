8c44254bcf878bb75d99cf00d4f240bab0903210
Karim Sami <karimsami047@gmail.com>
2022-01-07 21:56:21 +0200

    Implement an endpoint to add tours

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const HTTP_RESP_STATUS = require('./constants/http-resp-status');
+
+app.use(express.json()); //this is a middle waire provided by express library and used by the app to parse the request body on the request object
+
 const port = 8000;
 
 const tours = JSON.parse(
@@ -14,6 +17,28 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: { tours },
   });
+});
+
+app.post('/api/v1/tours/', (req, res) => {
+  const tour = req.body;
+  tour['id'] = tours.length;
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify([...tours, tour]),
+    (err) => {
+      if (err) {
+        console.log(e);
+        res.status(500).json({
+          status: HTTP_RESP_STATUS.ERROR,
+        });
+      }
+      res.status(201).json({
+        status: HTTP_RESP_STATUS.SUCCESS,
+        data: tour,
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
