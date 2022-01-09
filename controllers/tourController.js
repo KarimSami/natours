614cd08f3ed6e3efd2350d6bc9d3cf0c38d6ae59
Karim Sami <karimsami047@gmail.com>
2022-01-09 20:54:33 +0200

    Chain a middleware to the post middleware handler to check the tours request body

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,20 @@ const tourServices = require('../services/tourService');
 
 router.param('id', tourServices.checkId);
 
-router.route('/').get(tourServices.getAllTours).post(tourServices.createTour);
+const checkBody = (req, res, next) => {
+  const body = req.body;
+  if (!(body.name && body.price))
+    return res.status(400).json({
+      status: 'error',
+      message: 'Body is incomplete',
+    });
+  next();
+};
+
+router
+  .route('/')
+  .get(tourServices.getAllTours)
+  .post(checkBody, tourServices.createTour);
 router.route('/:id').get(tourServices.getTour).patch(tourServices.updateTour);
 
 module.exports = router;
