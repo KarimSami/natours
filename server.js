8ff696ab18f1ef32cb0ff2a07a8fa0b6502bb10f
Karim Sami <karimsami047@gmail.com>
2022-01-14 16:33:44 +0200

    Create a tour schema and build a model out of it

--- server.js
+++ server.js
@@ -8,6 +8,25 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() => console.log('Database connected successfully'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}`);
 });
