ca1df2ddc303c384ea11727462984f2e39473ee9
Karim Sami <karimsami047@gmail.com>
2022-01-16 22:45:07 +0200

    Add more fields to the tours model

--- models/tour.model.js
+++ models/tour.model.js
@@ -7,15 +7,48 @@ const tourSchema = mongoose.Schema(
       required: [true, 'A tour must have a name!'],
       unique: true,
     },
+    duration: {
+      type: Number,
+      required: [true, 'Atour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Atour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Atour must have a difficulty'],
+    },
     price: {
       type: Number,
       required: [true, 'A tour must have a price!'],
     },
-
-    rating: {
+    ratingsAverage: {
       type: Number,
       default: 4.5,
     },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    startDates: {
+      type: [Date],
+    },
+    images: [String],
   },
   { timestamps: true }
 );
