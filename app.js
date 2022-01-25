f593c90b2c7511ea15be0eb4ea022d1cfb963df5
Karim Sami <karimsami047@gmail.com>
2022-01-26 00:10:12 +0200

    Add fail safe handler for all routes not defined in our routers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const res = require('express/lib/response');
 const morgan = require('morgan');
+const HTTP_RESP_STATUS = require('./constants/http-resp-status');
 const tourRouter = require('./router/tour.router');
 const app = express();
 
@@ -13,5 +15,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 // app.use('/api/v1/users', userController);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: HTTP_RESP_STATUS.FAIL,
+    message: `can't find ${req.originalUrl}`,
+  });
+});
 
 module.exports = app;
