c2492748d9995d1edaaa93521a152d6d7b8c5690
Karim Sami <karimsami047@gmail.com>
2022-04-09 02:08:54 +0200

    Add uncaught exception handler

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+process.on('unhandledRejection', (err) => {
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('uncaughtException, shuting down');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
@@ -11,10 +24,4 @@ mongoose
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}`);
-});
-
-process.on('unhandledRejection', (err) => {
-  server.close(() => {
-    process.exit(1);
-  });
 });
