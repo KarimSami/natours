a1f74c179816868d2ba78bf26214e9e3c3bd9b2b
KarimSami <karim.sami@talentsarena.net>
2022-06-20 18:46:12 +0200

    Add a service to authenticate users

--- services/authService.js
+++ services/authService.js
@@ -1,5 +1,15 @@
+const { issueToken } = require('../utils/issue-token');
 const User = require('../models/user.model');
+const AppError = require('../utils/app-error');
 
 exports.createUser = async (userData) => {
   return await User.create(userData);
+};
+
+exports.authenticateUser = async (email, password) => {
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password)))
+    throw new AppError('Incorrect email or password');
+  return issueToken(user._id);
 };
