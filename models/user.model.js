4db63a73bc7bbffa2bf93bed993eba8cf8c8a3f2
KarimSami <karim.sami@talentsarena.net>
2022-06-20 18:45:51 +0200

    Add a user model method to verify a correct password

--- models/user.model.js
+++ models/user.model.js
@@ -25,6 +25,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A user must have a password'],
       minlength: 8,
+      select: false,
     },
     passwordConfirm: {
       type: String,
@@ -64,5 +65,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 const User = mongoose.model('user', userSchema);
 module.exports = User;
