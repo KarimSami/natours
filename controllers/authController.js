b21e384da4d57cfda0b93996cf6559d11a6571e6
KarimSami <karim.sami@talentsarena.net>
2022-06-20 18:45:26 +0200

    Add an endpoint to sign users in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,39 @@
 const HTTP_RESP_STATUS = require('../constants/http-resp-status');
 const authService = require('../services/authService');
 const catchAsync = require('../utils/catch-async');
+const AppError = require('../utils/app-error');
+const { issueToken } = require('../utils/issue-token');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await authService.createUser(req.body);
+  const newUser = await authService.createUser({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = issueToken(newUser._id);
+
   res.status(201).json({
     status: HTTP_RESP_STATUS.SUCCESS,
+    token,
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.signin = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  const token = await authService.authenticateUser(email, password);
+  if (!token)
+    return next(new AppError('Username or password is incorrect', 400));
+  res.status(200).json({
+    status: HTTP_RESP_STATUS.SUCCESS,
+    token,
   });
 });
